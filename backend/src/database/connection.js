9c3e6d57dc9b8ac35837041e6194da0ba48c25a0
Rnchx <jpcrocha8@gmail.com>
2025-10-29 21:07:59 -0300

    changes in html and css, and create new windows of student

--- backend/src/database/connection.js
+++ backend/src/database/connection.js
@@ -1,11 +1,24 @@
 const mysql = require('mysql2/promise');
 require('dotenv').config();
 
+// const connection = mysql.createPool({
+//     host: process.env.DB_HOST || 'localhost',
+//     user: process.env.DB_USER || 'root', 
+//     password: process.env.DB_PASSWORD || 'SeltR150610',
+//     database: process.env.DB_NAME || 'pi2',
+//     port: process.env.DB_PORT || 3306,
+//     waitForConnections: true,
+//     connectionLimit: 10,
+//     queueLimit: 0
+// });
+
+// conexao puc
+
 const connection = mysql.createPool({
-    host: process.env.DB_HOST || 'localhost',
-    user: process.env.DB_USER || 'root', 
-    password: process.env.DB_PASSWORD || 'SeltR150610',
-    database: process.env.DB_NAME || 'pi2',
+    host: process.env.DB_HOST || '172.16.12.14',
+    user: process.env.DB_USER || 'BD240225246', 
+    password: process.env.DB_PASSWORD || 'Gcgts5',
+    database: process.env.DB_NAME || 'BD240225246',
     port: process.env.DB_PORT || 3306,
     waitForConnections: true,
     connectionLimit: 10,
